5aa0bcb535681824c5a3fba59c19f2d52cab54e3
Sitao Chen <sitaochen@Sitaos-MacBook-Air.local>
2017-11-09 18:54:14 -0500

    input modified

--- src/library/input.py
+++ src/library/input.py
@@ -3,13 +3,26 @@ from node import Node
 class input(Node):
 	"""docstring for input"""
 
-	id = 0;
+	id = 0
 
-	def __init__(self, name, id, targets = None):
+	def __init__(self, name, id, targets = None, order = 20, general_para = 0.001):
 		global id
 		super().__init__(name, 'I' + str(id))
 		id += 1
 		if targets not in None:
 			for t in targets:
 				self.targets.add(t)
-		
+		self.input = 0
+		#function: (power, parameter)
+		self.function = set()
+		for i in range(order):
+			self.function.add((i, general_para))
+		self.result = 0
+	
+	def add_target(self, ref):
+		super().add_target(ref)
+
+	def calc_result(self, input):
+		self.result = 0
+		for pair in self.function:
+			self.result += input ** pair[0] * pair[1]
